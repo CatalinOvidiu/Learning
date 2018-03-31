df7aeaeb48c7a53c3263f8566d172f9cc45057db
CatalinOvidiu <37024482+CatalinOvidiu@users.noreply.github.com>
2018-03-31 11:31:23 +0300

    solved Keyword error from when deleting an employee or department

---
+++ test2.py
@@ -0,0 +1,23 @@
+
+def fizz_buzz(number):
+    if number % 2 == 0 and number % 3 == 0:
+        print('FizzBuzz')
+    elif number % 3 == 0:
+        print('Buzz')
+    elif number % 2 ==0:
+        print('Fizz')
+    else:
+        print ('Numarul nu este divisibil nici cu 2 si nici cu 3')
+
+fizz_buzz(19997)
+
+
+
+
+
+
+
+
+
+
+
