3245f95922a755060bf3b98664f93006317b0634
CatalinOvidiu <37024482+CatalinOvidiu@users.noreply.github.com>
2018-04-03 18:36:11 +0300

    solved Keyword error from when deleting an employee or department

--- test.py
+++ test.py
@@ -1,29 +1,21 @@
-class Department:
-    def __init__(self, name, email, schedule):
-        """
-        :param str name: department's name
-        :param str email: department's email
-        :param dict[list[list[datetime]]] schedule: The public schedule. A dict, mapping day names
-            to lists of hours between which the department is open for business publicly
-            Example: {"monday": [["08:00", "12:30"], ["13:30", "20:00"]], ..., "friday": ["08:00", "14:00"]}
-        """
-        self.name = name
-        self.email = email
-        self.schedule = schedule
 
-class Employee:
-    def __init__(self, name, level, birth_date, address, phone, department):
-        """
-        :param str name: employee's name
-        :param str level: either "manager" or "basic"
-        :param datetime.datetime birth_date: employee's birth date
-        :param str address: their address
-        :param str phone: the employee's phone number
-        :param Department department: the employee's department
-        """
-        self.name = name
-        self.level = level
-        self.birth_date = birth_date
-        self.address = address
-        self.phone = phone
-        self.department = department
+matrix = [
+[1,2,3],
+[3,4,5],
+[5,6,7],
+[7,8,9]
+]
+def flip_matrix_old_fashio(matrix):
+    m = len(matrix)
+    n = len(matrix[0])
+    
+    result = []
+    for i in range(n):
+        current_row = []
+        for j in range(m):
+            current_row.append(matrix[j][i])
+        result.append(current_row)
+    return result
+
+print(flip_matrix_old_fashio(matrix))
+
